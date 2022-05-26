7e0598d6cf0c3ff32784ba051ec1659312f4ee3a
Sbeltran16 <sbeltran16@gmail.com>
2022-05-25 17:24:47 -0700

    add items to popular page and use ratings in the popular cards

--- src/components/PopularItems/PopularItems.jsx
+++ src/components/PopularItems/PopularItems.jsx
@@ -1,14 +1,69 @@
 import React from "react";
 import { Reveal, Icon, Button, Card, Image } from "semantic-ui-react";
+import Rating from "../Rating/Rating"
 import ".//PopularItems.css"
 
 const PopularPage = () => (
 <>
-  <h2>Pokemon</h2>
-  <Card.Group>
+  <h2 className="pokemon"><a className="popLinks" href="https://www.pokemon.com/us/pokemon-tcg/">Pokemon</a></h2>
+  <Card.Group itemsPerRow={5}>
     <Card>
     <Image
-        src="https://cdn.shopify.com/s/files/1/0903/7868/products/Pokemon-Pikachu-60-64-Jungle-Unlimited-1999-Wizards-WOTC-012221_1_large.jpg?v=1611385119"
+        src="https://assets.pokemon.com/assets/cms2/img/cards/web/PL2/PL2_EN_113.png"
+        wrapped
+        ui={false}
+      />
+      <Card.Content>
+        <Card.Header>Flying Pikachu</Card.Header>
+        <Card.Description>
+          <a href="https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/platinum-series/pl2/113/">Card Details</a>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          22 Friends
+        </a>
+      </Card.Content>
+    </Card>
+    <Card>
+    <Image
+        src="https://assets.pokemon.com/assets/cms2/img/cards/web/SWSH35/SWSH35_EN_79.png"
+        wrapped
+        ui={false}
+      />
+      <Card.Content>
+        <Card.Header>Charizard V</Card.Header>
+        <Card.Description>
+          <a href="https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/ss-series/swsh35/79/">Card Details</a>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          22 Friends
+        </a>
+      </Card.Content>
+    </Card>
+    <Card>
+    <Image
+        src="https://i.ebayimg.com/images/g/sTUAAOSwpfBf7vAq/s-l400.jpg"
+        wrapped
+        ui={false}
+      />
+      <Card.Content>
+        <Card.Header>Meowth VMax</Card.Header>
+        <Card.Description>
+         <a href="https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/ss-series/swshp/SWSH005/">Card Details</a>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Rating />
+      </Card.Content>
+    </Card>
+    <Card>
+    <Image
+        src="https://assets.pokemon.com/assets/cms2/img/cards/web/SWSH4/SWSH4_EN_188.png"
         wrapped
         ui={false}
       />
@@ -30,142 +85,7 @@ const PopularPage = () => (
     </Card>
     <Card>
     <Image
-        src="https://52f4e29a8321344e30ae-0f55c9129972ac85d6b1f4e703468e6b.ssl.cf2.rackcdn.com/products/pictures/1105091.jpg"
-        wrapped
-        ui={false}
-      />
-      <Card.Content>
-        <Card.Header>Matthew</Card.Header>
-        <Card.Meta>
-          <span className="date">Joined in 2015</span>
-        </Card.Meta>
-        <Card.Description>
-          Matthew is a musician living in Nashville.
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <a>
-          <Icon name="user" />
-          22 Friends
-        </a>
-      </Card.Content>
-    </Card>
-    <Card>
-    <Image
-        src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
-        wrapped
-        ui={false}
-      />
-      <Card.Content>
-        <Card.Header>Matthew</Card.Header>
-        <Card.Meta>
-          <span className="date">Joined in 2015</span>
-        </Card.Meta>
-        <Card.Description>
-          Matthew is a musician living in Nashville.
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <a>
-          <Icon name="user" />
-          22 Friends
-        </a>
-      </Card.Content>
-    </Card>
-    <Card>
-    <Image
-        src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
-        wrapped
-        ui={false}
-      />
-      <Card.Content>
-        <Card.Header>Matthew</Card.Header>
-        <Card.Meta>
-          <span className="date">Joined in 2015</span>
-        </Card.Meta>
-        <Card.Description>
-          Matthew is a musician living in Nashville.
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <a>
-          <Icon name="user" />
-          22 Friends
-        </a>
-      </Card.Content>
-    </Card>
-  </Card.Group>
-  <h2>Music</h2>
-  <Card.Group>
-    <Card>
-    <Image
-        src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
-        wrapped
-        ui={false}
-      />
-      <Card.Content>
-        <Card.Header>Matthew</Card.Header>
-        <Card.Meta>
-          <span className="date">Joined in 2015</span>
-        </Card.Meta>
-        <Card.Description>
-          Matthew is a musician living in Nashville.
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <a>
-          <Icon name="user" />
-          22 Friends
-        </a>
-      </Card.Content>
-    </Card>
-    <Card>
-    <Image
-        src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
-        wrapped
-        ui={false}
-      />
-      <Card.Content>
-        <Card.Header>Matthew</Card.Header>
-        <Card.Meta>
-          <span className="date">Joined in 2015</span>
-        </Card.Meta>
-        <Card.Description>
-          Matthew is a musician living in Nashville.
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <a>
-          <Icon name="user" />
-          22 Friends
-        </a>
-      </Card.Content>
-    </Card>
-    <Card>
-    <Image
-        src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
-        wrapped
-        ui={false}
-      />
-      <Card.Content>
-        <Card.Header>Matthew</Card.Header>
-        <Card.Meta>
-          <span className="date">Joined in 2015</span>
-        </Card.Meta>
-        <Card.Description>
-          Matthew is a musician living in Nashville.
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <a>
-          <Icon name="user" />
-          22 Friends
-        </a>
-      </Card.Content>
-    </Card>
-    <Card>
-    <Image
-        src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+        src="https://m.media-amazon.com/images/I/81ai6zx6eXL._AC_SY879_.jpg"
         wrapped
         ui={false}
       />
@@ -186,11 +106,11 @@ const PopularPage = () => (
       </Card.Content>
     </Card>
   </Card.Group>
-  <h2>Movies</h2>
-  <Card.Group>
+  <h2 className="Music"><a className="popLinks" href="https://www.billboard.com/">Music</a></h2>
+  <Card.Group itemsPerRow={5}>
     <Card>
     <Image
-        src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+        src="https://m.media-amazon.com/images/I/61Ia07wdZQL._SL1500_.jpg"
         wrapped
         ui={false}
       />
@@ -212,7 +132,7 @@ const PopularPage = () => (
     </Card>
     <Card>
     <Image
-        src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+        src="https://i.pinimg.com/originals/2b/b4/ee/2bb4ee4b7b3cbb4c2e8412360d9dc35f.jpg"
         wrapped
         ui={false}
       />
@@ -234,7 +154,29 @@ const PopularPage = () => (
     </Card>
     <Card>
     <Image
-        src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+        src="https://media.pitchfork.com/photos/6076fd2e17d37fe4717d4907/1:1/w_600/Olivia-Rodrigo-SOUR.jpeg"
+        wrapped
+        ui={false}
+      />
+      <Card.Content>
+        <Card.Header>Olivia Rodrigo "Sour"</Card.Header>
+        <Card.Meta>
+          <span className="date">Price $19.98</span>
+        </Card.Meta>
+        <Card.Description>
+            <a href="https://genius.com/albums/Olivia-rodrigo/Sour">Album Details</a>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          22 Friends
+        </a>
+      </Card.Content>
+    </Card>
+    <Card>
+    <Image
+        src="https://media.pitchfork.com/photos/627c1023d3c744a67a846260/1:1/w_600/Kendrick-Lamar-Mr-Morale-And-The-Big-Steppers.jpg"
         wrapped
         ui={false}
       />
@@ -256,7 +198,120 @@ const PopularPage = () => (
     </Card>
     <Card>
     <Image
-        src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+        src="https://m.media-amazon.com/images/I/81ai6zx6eXL._AC_SY879_.jpg"
+        wrapped
+        ui={false}
+      />
+      <Card.Content>
+        <Card.Header>Matthew</Card.Header>
+        <Card.Meta>
+          <span className="date">Joined in 2015</span>
+        </Card.Meta>
+        <Card.Description>
+          Matthew is a musician living in Nashville.
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          22 Friends
+        </a>
+      </Card.Content>
+    </Card>
+  </Card.Group>
+  <h2 className="Movies"><a className="popLinks" href="https://www.rottentomatoes.com">Movies</a></h2>
+  <Card.Group itemsPerRow={5}>
+    <Card>
+    <Image
+        src="https://www.the-numbers.com/images/movies/opusdata/Wonder-Woman-1984-(2020).jpg"
+        wrapped
+        ui={false}
+      />
+      <Card.Content>
+        <Card.Header>Matthew</Card.Header>
+        <Card.Meta>
+          <span className="date">Joined in 2015</span>
+        </Card.Meta>
+        <Card.Description>
+          Matthew is a musician living in Nashville.
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          22 Friends
+        </a>
+      </Card.Content>
+    </Card>
+    <Card>
+    <Image
+        src="https://www.themoviedb.org/t/p/original/9ltcubNi8RaTirTjW0ZAOgmUb9v.jpg"
+        wrapped
+        ui={false}
+      />
+      <Card.Content>
+        <Card.Header>Matthew</Card.Header>
+        <Card.Meta>
+          <span className="date">Joined in 2015</span>
+        </Card.Meta>
+        <Card.Description>
+          Matthew is a musician living in Nashville.
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          22 Friends
+        </a>
+      </Card.Content>
+    </Card>
+    <Card>
+    <Image
+        src="https://www.the-numbers.com/images/movies/opusdata/Fast-and-Furious-9-(2020).jpg"
+        wrapped
+        ui={false}
+      />
+      <Card.Content>
+        <Card.Header>Matthew</Card.Header>
+        <Card.Meta>
+          <span className="date">Joined in 2015</span>
+        </Card.Meta>
+        <Card.Description>
+          Matthew is a musician living in Nashville.
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          22 Friends
+        </a>
+      </Card.Content>
+    </Card>
+    <Card>
+    <Image
+        src="https://m.media-amazon.com/images/I/81ai6zx6eXL._AC_SY879_.jpg"
+        wrapped
+        ui={false}
+      />
+      <Card.Content>
+        <Card.Header>Matthew</Card.Header>
+        <Card.Meta>
+          <span className="date">Joined in 2015</span>
+        </Card.Meta>
+        <Card.Description>
+          Matthew is a musician living in Nashville.
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name="user" />
+          22 Friends
+        </a>
+      </Card.Content>
+    </Card>
+    <Card>
+    <Image
+        src="https://m.media-amazon.com/images/I/81ai6zx6eXL._AC_SY879_.jpg"
         wrapped
         ui={false}
       />
--- src/pages/PopularPage/PopularPage.jsx
+++ src/pages/PopularPage/PopularPage.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import PopularItems from "../../components/PopularItems/PopularItems"
 import Footer from '../../components/Footer/Footer'
 import PageHeader from '../../components/Header/Header'
+import {Icon} from 'semantic-ui-react'
 import ".//PopularPage.css"
 
 export default function PopularPage({handleLogout, user}) {
   return (
       <>
         <PageHeader handleLogout={handleLogout} user={user}/>
-        <h1>Popular</h1>
+        <h1 className="popularTitle"><Icon name='fire' color="orange" size='Large' />Popular</h1>
         <PopularItems /><br/>
         <Footer/>
         </>
